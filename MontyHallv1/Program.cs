5e313c4df41ab5d05122da21e209fd8d702f2f33
Tom Hatcher <tom.hatcher@myob.com>
2020-11-13 10:35:00 +1300

    Reworked pseudo-code with steps for the logic of the game.

--- MontyHallv1/Program.cs
+++ MontyHallv1/Program.cs
@@ -11,10 +11,11 @@ namespace MontyHallv1
         }
     }
 }
-// TODO: Display message with three doors.
+// TODO: Generate three doors.
+// TODO: Assign prizes to doors.
 // TODO: Allow player to select door.
 // TODO: Show door with joke prize.
-// TODO: Give player option to change door
+// TODO: Give player option to change door.
 // TODO: Open door to reveal prize.
 // TODO: Record player selection, prize success
 // TODO: Loop through for 1000 cycles.
