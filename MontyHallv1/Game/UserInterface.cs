f544170e1bcca3f68c89896bb5ad12a213af76c1
Tom Hatcher <tom.hatcher@myob.com>
2020-11-30 14:22:32 +1300

    Refactored UserInterface class to use doorchange validation logic. Added in console messages to show which door has been selected.

--- MontyHallv1/Game/UserInterface.cs
+++ MontyHallv1/Game/UserInterface.cs
@@ -7,28 +7,38 @@ namespace MontyHallv1
         public void Game()
         {
             Console.WriteLine("Welcome to Monty Hall \n");
-            var isValidEntry = false;
+            var isValidUserDoorSelectionInput = false;
             var enteredDoor = "";
+            var isValidUserChangeDoorInput = false;
             var changeDoor = "";
-            while (!isValidEntry)
+            while (!isValidUserDoorSelectionInput)
             {
                 Console.WriteLine($"Please choose a door: {PrizeDoors.one}, {PrizeDoors.two}, {PrizeDoors.three}");
                 enteredDoor = Console.ReadLine();
                 
-                isValidEntry = Validation.UserEntry(enteredDoor);
+                isValidUserDoorSelectionInput = Validation.UserEntry(enteredDoor);
             }
             var montyGame = new MontyHallGame(Validation.InputConversion(enteredDoor), new RandomPrizeDoorAssigner());
-            var AnnouncersChosenDoor = montyGame.AnnouncersDoor();
+            var announcersChosenDoor = montyGame.AnnouncersDoor();
             
             Console.WriteLine($"You selected door {montyGame.PlayerSelection}");
-            Console.WriteLine($"Monty has opened door {AnnouncersChosenDoor} to show a {montyGame.DoorPrizeStorage[AnnouncersChosenDoor]} prize!");
-            Console.WriteLine("Do you wish to change doors? Yes/No");
-            
-            changeDoor = Console.ReadLine();
+            Console.WriteLine($"Monty has opened door {announcersChosenDoor} to show a {montyGame.DoorPrizeStorage[announcersChosenDoor]} prize!");
+            while (!isValidUserChangeDoorInput)
+            {
+                Console.WriteLine("Do you wish to change doors? Yes/No");
+                changeDoor = Console.ReadLine();
+
+                isValidUserChangeDoorInput = Validation.ChangeDoorValidator(changeDoor);
+            }
             
             if (changeDoor.ToLower() == "yes")
             {
                 montyGame.ChangePlayerDoor();
+                Console.WriteLine($"You have changed to door {montyGame.PlayerSelection} \n");
+            }
+            else
+            {
+                Console.WriteLine($"You have stuck with door {montyGame.PlayerSelection} \n");
             }
         }
     }
